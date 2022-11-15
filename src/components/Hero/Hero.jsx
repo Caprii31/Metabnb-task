e0e8ef81be76619697af29bf88d6d8a136413227
Ahmed Kamal <street.dancer31@gmail.com>
2022-11-15 17:03:40 +0200

    hero-refactor

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './index.scss'
+import heroImg1 from '../../assets/images/hero1.png'
+import heroImg2 from '../../assets/images/hero2.png'
+import heroImg3 from '../../assets/images/hero3.png'
+import heroImg4 from '../../assets/images/hero4.png'
+
+function Hero() {
+  return (
+    <section id="hero">
+        <section id="hero__text">
+            <h1 id="hero__text--header">rent a <span>place</span> away from <span>home</span> in the <span>metaverse</span></h1>
+            <p id="hero__text--par">we provide you access to luxury and affordable houses <br /> in the metaverse, get a chance to turn your <br /> imagination to reality at your comfort zone</p>
+            <div id="hero__text--search">
+                <input type="text" />
+                <button className="hero__text--search--btn btn">search</button>
+            </div>
+        </section>
+        <section id="hero__images">
+            <img src={heroImg1} alt="" className="hero__images--img" />
+            <img src={heroImg2} alt="" className="hero__images--img" />
+            <img src={heroImg3} alt="" className="hero__images--img" />
+            <img src={heroImg4} alt="" className="hero__images--img" />
+        </section>
+    </section>
+  )
+}
+
+export default Hero
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
-import Main from '../Main/Main'
+import Hero from '../Hero/Hero'
 import './index.scss'
 
 
@@ -8,7 +8,7 @@ function Home() {
   return (
     <div id="home">
         <Navbar />
-        <Main />
+        <Hero />
     </div>
   )
 }
